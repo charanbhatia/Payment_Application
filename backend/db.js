74a1dc767f448cc14115a3e58aff59e95c7f33b7
charanbhatia <charanbhatia05@gmail.com>
2024-09-18 16:33:13 +0530

    userSchema made and connected

---
+++ backend/db.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 20,
+        unique: true,
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
+    },
+
+    firstname: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 20,
+        trim: true,
+
+    },
+
+    lastname: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 20,
+        trim: true,
+
+    },   
+})
+
+const User =  mongoose.model('User', userSchema);
+
+
+
+module.exports = {
+    User
+};
+ 
