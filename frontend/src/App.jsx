d6b7e3ccb64899e2cdda79635ae096527c55ded9
Charanjeet Bhatia <charanbhatia05@gmail.com>
2024-10-26 01:13:33 +0530

    frontend completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,46 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Signin} from "./pages/Signin";
-import {Signup} from "./pages/Signup";
-import {Dashboard} from "./pages/dashboard";
-import {SendMoney} from "./pages/SendMoney";
-
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+import PrivateRoute from "./components/auth/PrivateRourte";
+import Settings from "./pages/Settings";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-       <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/send"
+          element={
+            <PrivateRoute>
+              <SendMoney />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <PrivateRoute>
+              <Settings />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
+export default App;
